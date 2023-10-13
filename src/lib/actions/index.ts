23136f5c32d988ee6454d84af59ae379ab153cb1
realfakenerd <lucas210898@hotmail.com>
2023-10-13 14:50:44 -0300

    fix(app): fix to blur load imgs

--- src/lib/actions/index.ts
+++ src/lib/actions/index.ts
@@ -1,7 +1,13 @@
 export function blur(node: HTMLElement) {
     const img = node.children.item(0) as HTMLImageElement;
-    img.style.transition = 'opacity 300ms ease-in-out'
+    img.style.transition = 'opacity 300ms ease-in-out';
     if (img.complete) {
+        img.style.opacity = '1';
+        node.classList.remove('animate-pulse');
+        node.style.backgroundImage = '';
+    }
+
+    img.onload = () => {
         img.style.opacity = '1';
         node.classList.remove('animate-pulse');
         node.style.backgroundImage = '';
